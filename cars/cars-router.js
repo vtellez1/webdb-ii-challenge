2a08104bce9cc5e2e4a35285943f5f47ed5cc8da
Vanessa <vanessatellez45@gmail.com>
2020-01-21 17:32:39 -0500

    set up migration

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -1,7 +1,9 @@
 const express = require('express');
 
-const db = require('');
+const db = require('../data/');
 
 const router = express.Router();
 
 // Insert CRUD here :)
+
+module.exports = router;
